a8125e73149d9f799500223591ffa88ec59dabf7
Armaghan-M <armaghan.m.dev@gmail.com>
2024-08-13 17:26:18 +0330

    getting input values as an object and adding them all to an array

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,19 @@ let form = document.querySelector("#form");
 let toFrom = document.querySelector("#tofrom");
 let details = document.querySelector("#details");
 let amount = document.querySelector("#amount");
+let allPayments = [];
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let newPayment = {
+        type: type.value,
+        toFrom: toFrom.value,
+        details: details.value,
+        amount: parseInt(amount.value),
+        isAdmin: true
+    };
+    allPayments.push(newPayment);
+    toFrom.value = "";
+    details.value = "";
+    amount.value = "";
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,28 @@ let form = document.querySelector("#form") as HTMLFormElement;
 let toFrom = document.querySelector("#tofrom") as HTMLInputElement;
 let details = document.querySelector("#details") as HTMLInputElement;
 let amount = document.querySelector("#amount") as HTMLInputElement;
+
+interface Payment {
+  type: string;
+  toFrom: string;
+  details: string;
+  amount: number;
+  isAdmin: boolean;
+}
+let allPayments: Payment[] = [];
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+  let newPayment: Payment = {
+    type: type.value,
+    toFrom: toFrom.value,
+    details: details.value,
+    amount: parseInt(amount.value),
+    isAdmin: true
+  };
+  allPayments.push(newPayment);
+
+  toFrom.value = "";
+  details.value = "";
+  amount.value = "";
+});
