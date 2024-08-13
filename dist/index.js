c326d33356aad32c93e6343f246479492fa3f5ad
Armaghan-M <armaghan.m.dev@gmail.com>
2024-08-13 11:21:05 +0330

    getting access to necessary elements of html file

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,7 @@
 "use strict";
 let type = document.querySelector("#type");
+let form = document.querySelector("#form");
+let toFrom = document.querySelector("#tofrom");
+let details = document.querySelector("#details");
+let amount = document.querySelector("#amount");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 let type = document.querySelector("#type") as HTMLSelectElement;
+let form = document.querySelector("#form") as HTMLFormElement;
+let toFrom = document.querySelector("#tofrom") as HTMLInputElement;
+let details = document.querySelector("#details") as HTMLInputElement;
+let amount = document.querySelector("#amount") as HTMLInputElement;
